681197a8d9e233d9b2e39cfb31f446a58eee4957
RaresAil <rares.ailincai@raresdesigns.com>
2024-03-25 12:49:12 +0200

    update configs

--- src/components/IonIcon.tsx
+++ src/components/IonIcon.tsx
@@ -1,8 +1,10 @@
 import { PureComponent } from "react";
 
+import { IonIconProps } from "./IonIcon.types";
+
 import "./IonIcon.css";
 
-export class IonIcon extends PureComponent {
+export class IonIcon extends PureComponent<IonIconProps> {
   render() {
     return <div>IonIcon</div>;
   }
