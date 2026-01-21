5083bbcb416712c0f05bda0303259f0020d12468
Kevin S <contact@k3vin.net>
2026-01-21 10:35:09 -0800

    [FIX] Fixes per copilot

--- rollup.config.js
+++ rollup.config.js
@@ -41,7 +41,7 @@ export default [
       ...Object.fromEntries(
         globSync('src/icons/*.tsx').map(file => [
           // This removes `src/` as well as the file extension from each
-          // file, so e.g. src/nested/foo.js becomes nested/foo
+          // file, so e.g. src/nested/foo.tsx becomes nested/foo
           path.relative('src', file.slice(0, file.length - path.extname(file).length)),
           // This expands the relative paths to absolute paths, so e.g.
           // src/nested/foo becomes /project/src/nested/foo.js
